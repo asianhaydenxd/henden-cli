165ab14afda74ddc6edc355ea8aa26b1c8530e9c
Hayden <asianhaydenxd@gmail.com>
2022-03-07 21:02:05 -0500

    Reorganize functions

--- main.py
+++ main.py
@@ -39,93 +39,6 @@ class Client(commands.Cog):
         curses.cbreak()
         curses.noecho()
     
-    def load_msgs(self):
-        os.system('cls' if os.name == 'nt' else 'clear') # Clear terminal for both Windows and Unix
-        print(f'\r{Fore.YELLOW}Chatting in{Fore.RESET}: {self.channel.guild.name}{Fore.YELLOW}/{Fore.RESET}{self.channel.name}')
-
-        if [message for message in self.unread_messages if message.channel != self.channel]:
-            last_foreign_message = [message for message in self.unread_messages if message.channel != self.channel][-1]
-            print(f'\r{Fore.GREEN}! {last_foreign_message.guild.name}{Fore.YELLOW}/{Fore.GREEN}{last_foreign_message.channel.name} {last_foreign_message.author.name}: {Fore.YELLOW}{last_foreign_message.content}{Fore.RESET}')
-        else:
-            print('\r')
-
-        lines = []
-        message_index = 0
-        while len(lines) < self.results:
-            if self.messages[message_index] not in self.messages[self.scroll : self.scroll + self.results]:
-                message_index += 1
-                continue
-                
-            message = self.messages[message_index]
-            message_index += 1
-            
-            color = discord.Color.greyple()
-            try:
-                for role in reversed(message.author.roles):
-                    if role.color.value != 0:
-                        color = role.color
-                        break
-            except AttributeError as e:
-                if e.name != 'roles': raise
-            finally:
-                message_lines = [f'\r\033[38;2;{color.r};{color.g};{color.b}m{message.author.display_name}{Fore.RESET}: {Fore.RED if message in self.unread_messages else Fore.RESET}' + message.content.split('\n')[0]] + message.content.replace('\n', '\n' + ' ' * (len(message.author.display_name) + 2)).split('\n')[1:]
-                message_lines.reverse()
-                lines += message_lines
-
-        print('\n'.join(list(reversed(lines[:self.results]))))
-                
-        
-        print('\r')
-
-    def load_guilds(self):
-        os.system('cls' if os.name == 'nt' else 'clear') # Clear terminal for both Windows and Unix
-        print(f'{Fore.GREEN}? Select server {Fore.RESET}({bot.guilds.index(self.guild)+1}/{len(bot.guilds)})\n')
-        for i, guild in enumerate(bot.guilds):
-            if i < bot.guilds.index(self.guild) - self.results/2 or i > bot.guilds.index(self.guild) + self.results/2:
-                continue
-                
-            if self.guild == guild:
-                if guild in [message.guild for message in self.unread_messages]:
-                    print(Fore.BLUE + '> ' + Fore.GREEN + guild.name + ' !' + Fore.RESET)
-                else:
-                    print(Fore.BLUE + '> ' + guild.name + Fore.RESET)
-            elif guild in [message.guild for message in self.unread_messages]:
-                print(Fore.GREEN + '  ' + guild.name + ' !' + Fore.RESET)
-            else:
-                print('  ' + guild.name)
-
-    def load_channels(self):
-        os.system('cls' if os.name == 'nt' else 'clear') # Clear terminal for both Windows and Unix
-        print(f'{Fore.GREEN}? Select channel {Fore.RESET}({self.guild.text_channels.index(self.channel)+1}/{len(self.guild.text_channels)})\n')
-        for i, channel in enumerate(self.guild.text_channels):
-            if i < self.guild.text_channels.index(self.channel) - self.results/2 or i > self.guild.text_channels.index(self.channel) + self.results/2:
-                continue
-            
-            if self.channel == channel:
-                if channel in [message.channel for message in self.unread_messages]:
-                    print(Fore.BLUE + '> ' + Fore.GREEN + channel.name + ' !' + Fore.RESET)
-                else:
-                    print(Fore.BLUE + '> ' + channel.name + Fore.RESET)
-            elif channel in [message.channel for message in self.unread_messages]:
-                print(Fore.GREEN + '  ' + channel.name + ' !' + Fore.RESET)
-            else:
-                print('  ' + channel.name)
-    
-    def end_curses(self):
-        self.screen.keypad(False)
-        curses.nocbreak()
-        curses.echo()
-
-        curses.endwin()
-    
-    async def ainput(self) -> str:
-        with ThreadPoolExecutor(1, 'ainput') as executor:
-            return (await asyncio.get_event_loop().run_in_executor(executor, input)).rstrip()
-
-    async def agetch(self) -> str:
-        with ThreadPoolExecutor(1, 'agetch') as executor:
-            return (await asyncio.get_event_loop().run_in_executor(executor, self.getch.impl)).rstrip()
-    
     @commands.Cog.listener()
     async def on_ready(self):
         print('Initiated HaydBot')
@@ -220,6 +133,93 @@ class Client(commands.Cog):
                 self.messages = await self.channel.history().flatten()
 
             self.load_msgs()
+
+    def load_guilds(self):
+        os.system('cls' if os.name == 'nt' else 'clear') # Clear terminal for both Windows and Unix
+        print(f'{Fore.GREEN}? Select server {Fore.RESET}({bot.guilds.index(self.guild)+1}/{len(bot.guilds)})\n')
+        for i, guild in enumerate(bot.guilds):
+            if i < bot.guilds.index(self.guild) - self.results/2 or i > bot.guilds.index(self.guild) + self.results/2:
+                continue
+                
+            if self.guild == guild:
+                if guild in [message.guild for message in self.unread_messages]:
+                    print(Fore.BLUE + '> ' + Fore.GREEN + guild.name + ' !' + Fore.RESET)
+                else:
+                    print(Fore.BLUE + '> ' + guild.name + Fore.RESET)
+            elif guild in [message.guild for message in self.unread_messages]:
+                print(Fore.GREEN + '  ' + guild.name + ' !' + Fore.RESET)
+            else:
+                print('  ' + guild.name)
+
+    def load_channels(self):
+        os.system('cls' if os.name == 'nt' else 'clear') # Clear terminal for both Windows and Unix
+        print(f'{Fore.GREEN}? Select channel {Fore.RESET}({self.guild.text_channels.index(self.channel)+1}/{len(self.guild.text_channels)})\n')
+        for i, channel in enumerate(self.guild.text_channels):
+            if i < self.guild.text_channels.index(self.channel) - self.results/2 or i > self.guild.text_channels.index(self.channel) + self.results/2:
+                continue
+            
+            if self.channel == channel:
+                if channel in [message.channel for message in self.unread_messages]:
+                    print(Fore.BLUE + '> ' + Fore.GREEN + channel.name + ' !' + Fore.RESET)
+                else:
+                    print(Fore.BLUE + '> ' + channel.name + Fore.RESET)
+            elif channel in [message.channel for message in self.unread_messages]:
+                print(Fore.GREEN + '  ' + channel.name + ' !' + Fore.RESET)
+            else:
+                print('  ' + channel.name)
+    
+    def load_msgs(self):
+        os.system('cls' if os.name == 'nt' else 'clear') # Clear terminal for both Windows and Unix
+        print(f'\r{Fore.YELLOW}Chatting in{Fore.RESET}: {self.channel.guild.name}{Fore.YELLOW}/{Fore.RESET}{self.channel.name}')
+
+        if [message for message in self.unread_messages if message.channel != self.channel]:
+            last_foreign_message = [message for message in self.unread_messages if message.channel != self.channel][-1]
+            print(f'\r{Fore.GREEN}! {last_foreign_message.guild.name}{Fore.YELLOW}/{Fore.GREEN}{last_foreign_message.channel.name} {last_foreign_message.author.name}: {Fore.YELLOW}{last_foreign_message.content}{Fore.RESET}')
+        else:
+            print('\r')
+
+        lines = []
+        message_index = 0
+        while len(lines) < self.results:
+            if self.messages[message_index] not in self.messages[self.scroll : self.scroll + self.results]:
+                message_index += 1
+                continue
+                
+            message = self.messages[message_index]
+            message_index += 1
+            
+            color = discord.Color.greyple()
+            try:
+                for role in reversed(message.author.roles):
+                    if role.color.value != 0:
+                        color = role.color
+                        break
+            except AttributeError as e:
+                if e.name != 'roles': raise
+            finally:
+                message_lines = [f'\r\033[38;2;{color.r};{color.g};{color.b}m{message.author.display_name}{Fore.RESET}: {Fore.RED if message in self.unread_messages else Fore.RESET}' + message.content.split('\n')[0]] + message.content.replace('\n', '\n' + ' ' * (len(message.author.display_name) + 2)).split('\n')[1:]
+                message_lines.reverse()
+                lines += message_lines
+
+        print('\n'.join(list(reversed(lines[:self.results]))))
+                
+        
+        print('\r')
+
+    def end_curses(self):
+        self.screen.keypad(False)
+        curses.nocbreak()
+        curses.echo()
+
+        curses.endwin()
+    
+    async def ainput(self) -> str:
+        with ThreadPoolExecutor(1, 'ainput') as executor:
+            return (await asyncio.get_event_loop().run_in_executor(executor, input)).rstrip()
+
+    async def agetch(self) -> str:
+        with ThreadPoolExecutor(1, 'agetch') as executor:
+            return (await asyncio.get_event_loop().run_in_executor(executor, self.getch.impl)).rstrip()
 
 def main():
     bot.add_cog(Client(bot))
